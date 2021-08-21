6354fab06280babe8dd777c40ea9524f68641da5
yogavanan <m_yogavanan@yahoo.co.in>
2021-08-21 22:46:14 +0530

    Processing each record with multiple lines working

--- TextToXML/TextToXml.py
+++ TextToXML/TextToXml.py
@@ -1,10 +1,36 @@
 import os, re
 
-#open the file in read mode
-with open ('input.txt', 'r') as reader:
-    # Read and print the entire file line by line
-    line = reader.readline()
-    while line != '':  # The EOF char is an empty string
-        print(line, end='')
-        line = reader.readline()
 
+def process_record(record_data):
+    print (record_data)
+
+#read the entire file line by line and find a record (key & value pair)
+def read_records():
+    #open the file in read mode
+    with open ('input.txt', 'r') as reader:
+
+        # Read and print the entire file line by line
+        current_line = reader.readline()
+
+        while current_line != '':  # The EOF char is an empty string
+
+            #store the current line in the record data
+            record_data=current_line
+            #print(current_line, end='')
+            next_line = reader.readline()
+
+            #check if record is on single line or multiple lines
+            if next_line != '' and next_line[0] == '$':
+                #process the record if it is fully read
+                process_record(record_data)
+            else:
+                #if it is a multiple line record. append the data
+                record_data += next_line
+
+            #move to next line
+            current_line = next_line
+                
+                
+
+
+read_records()
