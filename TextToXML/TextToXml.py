08b2d179b9f5951d998083d1c1fb55f6c00689d5
yogavanan <m_yogavanan@yahoo.co.in>
2021-08-21 21:53:43 +0530

    File read is working

---
+++ TextToXML/TextToXml.py
@@ -0,0 +1,10 @@
+import os, re
+
+#open the file in read mode
+with open ('input.txt', 'r') as reader:
+    # Read and print the entire file line by line
+    line = reader.readline()
+    while line != '':  # The EOF char is an empty string
+        print(line, end='')
+        line = reader.readline()
+
