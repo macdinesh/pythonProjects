e5ac2be9e618befb9ef88f6df4067c71a68f7088
yogavanan <m_yogavanan@yahoo.co.in>
2021-08-21 23:52:24 +0530

    template to push data to a class

--- TextToXML/TextToXml.py
+++ TextToXML/TextToXml.py
@@ -1,8 +1,51 @@
 import os, re
 
+class User:
+    def __init__(self):
+            self.user_name = ''
+            self.user_code =  ''
+            self.user_desc = ''
+            self.no_of_follow = ''
+            self.user_verified = ''
+            self.tweet_date = ''
+            self.tweet_list = ''
+            
+    def update_user_name(self, name):
+        self.user_name = name
 
+    def update_user_code(self, code):
+        self.user_code = code
+
+    def print_user(self):
+        print ("name = ", self.user_name)
+        print ("code = ", self.user_code)
+
+
+tempUser = User()
+
+def update_user_data(key, value):
+
+    if (key == '$uname.'):
+        print("username found", key, value)
+        tempUser.update_user_name(value)
+    elif (key == '$user_code.'):
+        print("user_code found", key, value)
+        tempUser.update_user_code(value)
+        
+    #process other tags
+    tempUser.print_user()
+
+
+#process each record and make key & value pair
 def process_record(record_data):
-    print (record_data)
+    #print (record_data)
+    #split the record to retrevive the key value pair
+    temp=record_data.split(":")
+    key= temp[0]
+    value=temp[1]
+    #print("key=", key , "value=", value);
+    update_user_data(key, value)
+    
 
 #read the entire file line by line and find a record (key & value pair)
 def read_records():
